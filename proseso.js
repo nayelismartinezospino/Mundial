4e8d6642e44303bb3d633f7f3ab0218fe0561150
jharold1018 <jharol1018@gmail.com>
2023-06-29 11:07:36 -0500

    first commit

---
+++ proseso.js
@@ -0,0 +1,11 @@
+
+function imagen() {
+   document.getElementById('irg').innerHTML=""
+   +"<img src='re esta.jpg' alt='' width=600' height='400'>";
+   
+}
+function imagen1() {
+   document.getElementById('irg1').innerHTML=""
+   +"<img src='recife.jpg' alt='' width=600' height='400'>";
+   
+}
